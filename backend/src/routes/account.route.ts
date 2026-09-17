e0fe6bed7d34d2b171bc74049bd956608ff2e269
EricHoang28092k5 <manhoang2809@gmail.com>
2026-09-17 10:08:34 +0700

    feat: soft delete, ràng buộc 1 admin, ko lock ko thêm admin

--- backend/src/routes/account.route.ts
+++ backend/src/routes/account.route.ts
@@ -14,4 +14,6 @@ router.get("/edit/:accountId", accountController.edit);
 
 router.patch("/edit/:accountId", accountValidate.validateUpdateAccount, accountController.editPatch);
 
+router.patch("/lock/:accountId", accountController.lock);
+
 export const accountRoutes = router;
