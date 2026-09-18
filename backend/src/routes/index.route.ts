cdfceab968c492ceb63b222b75c3570197aec85a
Nguyễn Tấn Dũng <156091609+Wolf0170@users.noreply.github.com>
2026-09-18 08:03:45 +0700

    Update route configuration

--- backend/src/routes/index.route.ts
+++ backend/src/routes/index.route.ts
@@ -6,18 +6,17 @@ import { importOrderRoutes } from "./import_orders.route";
 import { exportOrderRoutes } from "./export_orders.route";
 import { bomRoutes } from "./bom.route";
 
-const adminRoutes = (app: Express): void =>{
-    const PATH_ADMIN = `${systemConfig.prefixAdmin}`;
+const adminRoutes = (app: Express): void => {
+  const PATH_ADMIN = `${systemConfig.prefixAdmin}`;
 
-    app.use(`${PATH_ADMIN}/components`, componentRoutes);
+  app.use(`${PATH_ADMIN}/components`, componentRoutes);
 
-    app.use(`${PATH_ADMIN}/accounts`, accountRoutes);
+  app.use(`${PATH_ADMIN}/accounts`, accountRoutes);
 
-    app.use(`${PATH_ADMIN}/importOrders`, importOrderRoutes);
+  app.use(`${PATH_ADMIN}/importOrders`, importOrderRoutes);
 
-    app.use(`${PATH_ADMIN}/exportOrders`, exportOrderRoutes);
+  app.use(`${PATH_ADMIN}/exportOrders`, exportOrderRoutes);
 
-    app.use(`${PATH_ADMIN}/bom`, bomRoutes);
-
-}
+  app.use(`${PATH_ADMIN}/bom`, bomRoutes);
+};
 export default adminRoutes;
