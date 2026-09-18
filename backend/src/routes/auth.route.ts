45d1d6d185260b81d3f82679593d42479792e07c
EricHoang28092k5 <manhoang2809@gmail.com>
2026-09-18 09:59:01 +0700

    feat: đăng nhập thành công

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import * as authController from "../controllers/auth.controller";
+import * as authValidate from "../validates/auth.validate";
 
 const router = Router();
 
 router.get("/login", authController.login);
+
+router.post("/login", authValidate.loginValidate, authController.loginPost);
 
 export const authRoutes = router;
