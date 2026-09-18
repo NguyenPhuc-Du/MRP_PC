b38b4ffaea040b5bd163f2ed2ceae344182c94df
truong210705 <truongdeptrai2107@gmail.com>
2026-09-18 14:50:53 +0700

    fix loi du csdl

--- backend/prisma/migrations/20260918034130_add_component_deleted/migration.sql
+++ backend/prisma/migrations/20260918034130_add_component_deleted/migration.sql
@@ -3,4 +3,4 @@ ALTER TABLE "components" ADD COLUMN     "deleted" BOOLEAN NOT NULL DEFAULT false
 ADD COLUMN     "deleted_at" TIMESTAMP(3);
 
 -- AlterTable
-ALTER TABLE "roles" RENAME CONSTRAINT "roles_fixed_pkey" TO "roles_pkey";
+
