0ffa22cc478a9ffef9b5a29e846feaaac6637b6a
EricHoang28092k5 <manhoang2809@gmail.com>
2026-09-20 16:12:25 +0700

    feature: tạo logout

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as authService from "../services/auth.service";
+import { systemConfig } from "../config/system";
 
 export const login = async (req: Request, res: Response): Promise<void> => {
   res.render("pages/auth/login");
@@ -8,3 +9,9 @@ export const login = async (req: Request, res: Response): Promise<void> => {
 export const loginPost = async (req: Request, res: Response): Promise<void> => {
   await authService.login(res, req.body.username);
 };
+
+export const logout = async (req: Request, res: Response): Promise<void> => {
+  await authService.logout(res);
+
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+}
